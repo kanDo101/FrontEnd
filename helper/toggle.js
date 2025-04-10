3460e85db7860641642eec9eec1e63beccb02809
Adem-abderrazek <adem11.abderrazek@gmail.com>
2025-04-10 14:38:01 +0100

    working on presisting the theme throw all the pages

--- helper/toggle.js
+++ helper/toggle.js
@@ -1,14 +1,23 @@
 function toggleTheme() {
-    const currentTheme = document.body.getAttribute("data-theme");
-    if (currentTheme === "dark") {
-      document.body.removeAttribute("data-theme");
+  const currentTheme = document.body.getAttribute("data-theme");
 
-      document.querySelector(
-        ".theme-toggle"
-      ).innerHTML = `<i class="fas fa-moon"></i>`;
-    } else {
+  if (currentTheme === "dark") {
+      document.body.removeAttribute("data-theme");
+      localStorage.setItem("theme", "light");
+      document.querySelector(".theme-toggle").innerHTML = `<i class="fas fa-moon"></i>`;
+  } else {
       document.body.setAttribute("data-theme", "dark");
-      document.querySelector(".theme-toggle").innerHTML =
-        '<i class="fas fa-sun"></i>';
-    }
+      localStorage.setItem("theme", "dark");
+      document.querySelector(".theme-toggle").innerHTML = `<i class="fas fa-sun"></i>`;
   }
+}
+
+// Apply theme on page load
+document.addEventListener("DOMContentLoaded", function () {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+      document.body.setAttribute("data-theme", "dark");
+      document.querySelector(".theme-toggle").innerHTML = `<i class="fas fa-sun"></i>`;
+  }
+});
